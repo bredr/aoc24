dbc8a42aeb5a1384129ce432f8383eefd47a2377
bredr <davidbrett88@gmail.com>
2024-12-22 15:59:26 +0000

    day9 part2

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[aoc(day9, part1)]
 pub fn solve_part1(input: &str) -> u64 {
     let disk_map: Vec<u32> = input.chars().map(|x| x.to_digit(10).unwrap()).collect();
@@ -51,67 +53,75 @@ pub fn solve_part1(input: &str) -> u64 {
 #[aoc(day9, part2)]
 pub fn solve_part2(input: &str) -> u64 {
     let disk_map: Vec<u32> = input.chars().map(|x| x.to_digit(10).unwrap()).collect();
-    let mut block_sizes: Vec<(usize, u32)> = disk_map
-        .iter()
-        .step_by(2)
-        .enumerate()
-        .map(|(idx, x)| (idx, *x))
-        .collect();
-    let empty_sizes: Vec<(usize, u32)> = disk_map
-        .iter()
-        .skip(1)
-        .step_by(2)
-        .enumerate()
-        .map(|(idx, x)| (idx, *x))
-        .collect();
-    let mut disk: Vec<Vec<i32>> = disk_map
+    let file_sizes: HashMap<usize, u32> = HashMap::from_iter(
+        disk_map
+            .iter()
+            .step_by(2)
+            .enumerate()
+            .map(|(idx, x)| (idx, *x)),
+    );
+    let mut disk: Vec<i32> = disk_map
         .iter()
         .enumerate()
-        .map(|(idx, x)| {
-            (0..*x)
-                .map(move |_| {
-                    let idx = idx.clone();
-                    match idx % 2 {
-                        0 => (idx / 2) as i32,
-                        _ => -1,
-                    }
-                })
-                .collect()
+        .flat_map(|(idx, x)| {
+            (0..*x).map(move |_| {
+                let idx = idx.clone();
+                match idx % 2 {
+                    0 => (idx / 2) as i32,
+                    _ => -1,
+                }
+            })
         })
         .collect();
 
-    block_sizes.reverse();
-    for (empty_idx, empty_size) in empty_sizes {
-        let mut stack = Vec::<(usize, u32)>::new();
-        for (idx, size) in block_sizes.clone() {
-            if stack.iter().map(|(_, s)| *s as u32).sum::<u32>() + size <= empty_size {
-                stack.push((idx, size));
-                disk[2 * idx] = (0..size).map(|_| -1).collect();
-            }
-            if stack.iter().map(|(_, s)| *s as u32).sum::<u32>() == empty_size {
-                break;
-            }
-        }
-        block_sizes = block_sizes
-            .iter()
-            .filter(|(id, _)| !stack.iter().any(|(oid, _)| oid == id))
-            .map(|x| *x)
-            .collect();
+    let max_file_id = *disk.iter().max().unwrap();
 
-        let mut stack_block: Vec<i32> = stack
-            .iter()
-            .flat_map(|(idx, size)| (0..*size).map(|_| *idx as i32))
-            .collect();
-        if stack_block.len() < empty_size as usize {
-            for _ in 0..(empty_size - stack_block.len() as u32) {
-                stack_block.push(-1);
+    for file_id in (0..max_file_id + 1).rev() {
+        let file_id: usize = file_id.try_into().unwrap();
+        if let Some(start_pos) = disk.iter().position(|&x| x == file_id as i32) {
+            let file_size = file_sizes.get(&file_id).unwrap();
+            let mut best_free_space: Option<usize> = None;
+            let mut i = 0;
+            while i < start_pos {
+                if disk[i] == -1 {
+                    let mut free_space_size = 0;
+                    let mut pos = i;
+                    loop {
+                        if pos == disk.len() {
+                            break;
+                        }
+                        if disk[pos] == -1 {
+                            free_space_size += 1;
+                        } else {
+                            break;
+                        }
+                        pos += 1;
+                    }
+                    if free_space_size >= *file_size {
+                        best_free_space = Some(i);
+                        break;
+                    }
+                    i += free_space_size as usize;
+                } else {
+                    i += 1;
+                }
+            }
+            if let Some(v) = best_free_space {
+                if v < start_pos {
+                    // clear old location
+                    for i in start_pos..(start_pos + *file_size as usize) {
+                        disk[i] = -1;
+                    }
+                    // place new location
+                    for i in v..(v + *file_size as usize) {
+                        disk[i] = file_id as i32;
+                    }
+                }
             }
         }
-        disk[empty_idx * 2 + 1] = stack_block;
     }
 
     disk.iter()
-        .flatten()
         .enumerate()
         .map(|(idx, id)| match *id {
             -1 => 0,
